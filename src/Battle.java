e2f387563b3f91a6315df9ba7d6d1030dcb40797
김대훈 <eogns136@gmail.com>
2025-12-22 19:30:52 +0900

    Add: 승패 메시지 출력 기능 구현완료, 필수 기능 구현 완료

--- src/Battle.java
+++ src/Battle.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Battle {
     private static Random random = new Random();
     public static void oneVsOne(Team<Champion> t1, Team<Champion> t2) {
-        Champion c1 = randomPick(t1.getMembers());
+        Champion c1 = randomPick(t1.getMembers());   //랜덤으로 각 팀의 챔피언 선택
         Champion c2 = randomPick(t2.getMembers());
 
         System.out.println("\n===== 1:1 전투 시작 =====");
@@ -21,6 +21,15 @@ public class Battle {
             c2.UseR(c2,c1);
         }catch (DeadChampionActionException e){
             System.out.println(e.getMessage());
+        }
+        System.out.println("========================");
+
+        if (c1.getHp() > c2.getHp()) {
+            System.out.println(c1.getName() + " 승!");
+        } else if (c1.getHp() < c2.getHp()) {
+            System.out.println(c2.getName() + " 승!");
+        } else{
+            System.out.println("무승부!");
         }
 
         System.out.println("========================");
